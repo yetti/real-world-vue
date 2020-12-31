f4edba3f52604e9a3eef32cd94b3e69e8477e628
Yetrina Battad <hello@yetti.io>
2021-01-01 10:02:23 +1100

    feat: adds EventDetails and dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -9,11 +10,14 @@ const routes = [
     component: EventList
   },
   {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: About
   }
 ]
